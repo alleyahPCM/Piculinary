049b92448ce2f535e3fbf66d210c59b2474bda42
alleyahPCM <apmmanalili@gmail.com>
2024-07-26 16:58:37 +0800

    Changed aaron_picture.jpg, Implemented History Navigation and Prepped History Album

--- app/src/main/java/com/example/piculinary/HistoryAlbumFragment.java
+++ app/src/main/java/com/example/piculinary/HistoryAlbumFragment.java
@@ -7,12 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class HistoryAlbumFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_album, container, false);
+        View view = inflater.inflate(R.layout.fragment_history_album, container, false);
+
+        // Find the back button and set the click listener
+        ImageView backButton = view.findViewById(R.id.arrowBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go back to the previous page/fragment
+                ((MainActivity) requireActivity()).navigateToFragment(new HistoryFragment());
+            }
+        });
+
+        return view;
     }
 }
